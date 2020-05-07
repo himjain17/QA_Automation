b2b05369764ce0e6cb02451551606d87c2e8ba62
Himanshu Jain <aryan.jain90@gmail.com>
2020-05-07 16:09:18 +0530

    Add Elements locators

--- pages/homePage.ts
+++ pages/homePage.ts
@@ -1,15 +1,34 @@
-import { ElementFinder, element, by } from "protractor";
+import { ElementFinder, element, by, browser } from "protractor";
+import { GlobalMethods } from "../utility/globalMethods";
 
+const globalMethods = new GlobalMethods();
 
 export class HomePage{
 
+    homeLink : ElementFinder
+
     register : ElementFinder
     signOn : ElementFinder
-
+    userNameInput : ElementFinder
+    passwordInput : ElementFinder
+    signInButton : ElementFinder
+    
     constructor(){
-        this.register = element(by.xpath('//a[contains(text(),"REGISTER")]')),
+        this.homeLink = element(by.xpath('//a[contains(text(),"Home")]'))
+
+        this.register = element(by.xpath('//a[contains(text(),"REGISTER")]'))
         this.signOn = element(by.xpath('//a[contains(text(),"SIGN-ON")]'))
+        this.userNameInput = element(by.css('input[name="userName"]'))
+        this.passwordInput = element(by.css('input[name="password"]'))
+        this.signInButton = element(by.css('input[name="login"]'))
         
+    }
+
+    loginToApplication(testData : any){
+        globalMethods.setValueToTextField(this.userNameInput,testData.userName)
+        globalMethods.setValueToTextField(this.passwordInput,testData.password)
+        browser.sleep(3000)
+        globalMethods.clickOnElement(this.signInButton)
     }
 
 
@@ -18,10 +37,8 @@ export class HomePage{
     }
 
 
-
-
-    navigateToSignOnPage = function(){
-
+    navigateToSignOnPage(){
+        this.signOn.click();
     }
 
 
