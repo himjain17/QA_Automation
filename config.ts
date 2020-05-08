1787d53b85eaf26b0f38ab1aab2f24e883bcd711
Himanshu Jain <aryan.jain90@gmail.com>
2020-05-08 16:12:34 +0530

    Update config.ts file

--- config.ts
+++ config.ts
@@ -1,6 +1,7 @@
+const Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter')
 import {Config, browser} from "protractor";
 
-export let config : Config ={
+export let config : Config = {
     directConnect : true,
     
     //seleniumAddress : 'http://localhost:4444/wd/hub',
@@ -14,6 +15,11 @@ export let config : Config ={
     onPrepare : function(){
         browser.waitForAngularEnabled(false);
         browser.manage().window().maximize();
+        browser.manage().timeouts().implicitlyWait(10000);
+        jasmine.getEnv().addReporter(new Jasmine2HtmlReporter({
+            savePath : 'target/screenshots'
+        })
+        );
     },
 
     jasmineNodeOpts : {
